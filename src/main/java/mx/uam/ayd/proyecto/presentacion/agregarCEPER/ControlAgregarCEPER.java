4437812451208f755e2b761b981652f3261a160b
Ulises Juarez <mmcjuarez@gmail.com>
2025-08-09 14:52:53 -0600

    Bateria CEPER incompleto pero funcional

--- src/main/java/mx/uam/ayd/proyecto/presentacion/agregarCEPER/ControlAgregarCEPER.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/agregarCEPER/ControlAgregarCEPER.java
@@ -4,14 +4,28 @@ import org.springframework.stereotype.Component;
 import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.annotation.PostConstruct;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+import java.util.Arrays;
+import java.util.List;
+
 import mx.uam.ayd.proyecto.negocio.ServicioBateriaClinica;
+import mx.uam.ayd.proyecto.negocio.modelo.TipoBateria;
 import mx.uam.ayd.proyecto.presentacion.agregarCEPER.VentanaAgregarCEPER;
+
+
 
 @Component
 public class ControlAgregarCEPER {
     
     private final VentanaAgregarCEPER ventanaAgregarCEPER;
     private final ServicioBateriaClinica servicioBateriaClinica;
+
+    private Long pacienteID;
 
     @Autowired
     public ControlAgregarCEPER(
@@ -21,12 +35,24 @@ public class ControlAgregarCEPER {
             this.servicioBateriaClinica=servicioBateriaClinica;
         }
     
-        @PostConstruct
-        public void inicializa(){
-            ventanaAgregarCEPER.setControlAgregarCEPER(this);
-        }
+    @PostConstruct
+    public void inicializa(){
+        ventanaAgregarCEPER.setControlAgregarCEPER(this);
+    }
 
-        public void inicia(){
-            ventanaAgregarCEPER.muestra();
-        }
+    public void inicia(Long pacienteID){
+        this.pacienteID=pacienteID;
+        ventanaAgregarCEPER.setControlAgregarCEPER(this);
+        ventanaAgregarCEPER.setPacienteID(pacienteID);
+        ventanaAgregarCEPER.muestra();
+    }
+
+    public void guardarCEPER(Long pacienteID, List<Integer> respuestas, String comentarios) {
+        servicioBateriaClinica.registrarBateria(
+            pacienteID,
+            TipoBateria.CEPER,
+            respuestas,
+            comentarios
+        );
+    }    
 }
